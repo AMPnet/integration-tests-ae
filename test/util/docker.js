8ce9460701dda3bba98c2a9f93249e0e337bcb06
Eugen Druzin <githinho@users.noreply.github.com>
2020-09-24 14:13:34 +0000

    Update test/util/docker.js
    Co-authored-by: Filip Dujmusic <38497167+filipdujmusic@users.noreply.github.com>

--- test/util/docker.js
+++ test/util/docker.js
@@ -36,7 +36,13 @@ async function healthcheck() {
     let walletServiceChecker = new HTTPChecker('Wallet Service checker', 'http://localhost:8128/actuator/health')
     let userServiceChecker = new HTTPChecker('User Service checker', 'http://localhost:8125/actuator/health')
     let reportServiceChecker = new HTTPChecker('Report Service checker', 'http://localhost:8129/actuator/health')
-    let healthcheck = new Healthcheck([blockchainServiceChecker, projectServiceChecker, walletServiceChecker, userServiceChecker, reportServiceChecker])
+let healthcheck = new Healthcheck([
+    blockchainServiceChecker,
+    projectServiceChecker,
+    walletServiceChecker,
+    userServiceChecker,
+    reportServiceChecker
+])
     var numberOfChecks = 0
     do {
         if (numberOfChecks >= maxNumberOfChecks) {
