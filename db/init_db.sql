de74b5ba157001dc63910516eef1ff8379fbc445
Eugen Druzin <edruzin@gmail.com>
2021-03-17 16:03:33 +0100

    Add pgcrypto extension to user_service database

--- db/init_db.sql
+++ db/init_db.sql
@@ -6,6 +6,8 @@ CREATE USER wallet_service WITH PASSWORD 'password';
 
 CREATE DATABASE user_service ENCODING 'UTF-8';
 CREATE USER user_service WITH PASSWORD 'password';
+\connect user_service
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE DATABASE ae_middleware_testnet ENCODING 'UTF-8';
 CREATE USER ae_middleware_testnet WITH PASSWORD 'password';
